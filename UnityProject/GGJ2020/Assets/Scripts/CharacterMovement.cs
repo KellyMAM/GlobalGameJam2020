1614e0bfda87fa0503a0175544471132e5c51e77
Ross Borchers <ross@renderheads.com>
2020-02-02 03:40:48 +0200

    Basic beaver and better camera

--- UnityProject/GGJ2020/Assets/Scripts/CharacterMovement.cs
+++ UnityProject/GGJ2020/Assets/Scripts/CharacterMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace GGJ
 {
-	[RequireComponent(typeof(Rigidbody))]
+	//[RequireComponent(typeof(Rigidbody))]
 	public class CharacterMovement : MonoBehaviour
 	{
 		public float Speed = 200f;
@@ -12,11 +12,11 @@ namespace GGJ
 
 		private float _vertical;
 		private float _horizontal;
-		private Rigidbody _rigidbody;
+		public Rigidbody Rigidbody;
 
 		private void Start()
 		{
-			_rigidbody = GetComponent<Rigidbody>();
+			//Rigidbody = GetComponent<Rigidbody>();
 		}
 
 		private void FixedUpdate()
@@ -25,11 +25,11 @@ namespace GGJ
 			_horizontal = Input.GetAxis("Horizontal");
 
 			Vector3 velocity = (transform.forward * _vertical) * Speed * Time.fixedDeltaTime;
-			velocity.y = _rigidbody.velocity.y;
-			_rigidbody.velocity = velocity;
+			velocity.y = Rigidbody.velocity.y;
+			Rigidbody.velocity = velocity;
 
 			Vector3 angularVelocity = (transform.up * _horizontal) * RotationSpeed * Time.fixedDeltaTime;
-			_rigidbody.angularVelocity = angularVelocity;
+			Rigidbody.angularVelocity = angularVelocity;
 		}
 	}
 }
