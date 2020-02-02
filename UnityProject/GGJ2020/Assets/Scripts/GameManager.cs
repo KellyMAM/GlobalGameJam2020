b7c04a1a1926b4f08af7fc280a34f62d181c9b51
Kelly McCarter <35454029+KellyMAM@users.noreply.github.com>
2020-02-02 11:32:14 +0200

    Textmesh pro

--- UnityProject/GGJ2020/Assets/Scripts/GameManager.cs
+++ UnityProject/GGJ2020/Assets/Scripts/GameManager.cs
@@ -4,8 +4,62 @@ using UnityEngine;
 
 namespace GGJ
 {
+	public enum GameState
+	{
+		Attractor,
+		Start,
+		Playing,
+		PayOff
+	}
+
 	public class GameManager : MonoBehaviour
 	{
-		///AND SO WE START
+		public GameState CurrentState = GameState.Attractor;
+
+		[SerializeField]
+		private Attractor _attractor;
+		[SerializeField]
+		private Level _levelPrefab;
+
+		private Level _currentLevel;
+
+		private void Awake()
+		{
+			_attractor.StartButton.onClick.AddListener(StartGame);
+		}
+
+		private void TurnOnAttractor()
+		{
+			_attractor.TurnOnAttractor();
+		}
+
+		private void TurnOffAttractor()
+		{
+			_attractor.TurnOffAttractor();
+		}
+
+		private void StartGame()
+		{
+			Debug.Log("Starting game");
+			_currentLevel = Instantiate<GameObject>(_levelPrefab.gameObject, this.transform).GetComponent<Level>();
+			_currentLevel.transform.position = Vector3.zero;
+			_currentLevel.DamCollector.OnAllLogSpacesFilled += PlayPayOff;
+
+			TurnOffAttractor();
+		}
+
+		private void EndGame()
+		{
+			TurnOnAttractor();
+			Destroy(_currentLevel.gameObject);
+			_currentLevel = null;
+		}
+
+		private void PlayPayOff()
+		{
+			Debug.Log("Well done you have completed the game");
+
+			EndGame();
+		}
 	}
 }
